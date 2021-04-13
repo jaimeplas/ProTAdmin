5ab64420301c0730d5cc80e9ce8d79feed96599f
jaimeplas <jimmypls_hm@hotmail.com>
2021-04-12 20:59:12 -0500

    Modificaciones en las Rutas y Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { APP_ROUTE } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register/register.component';
-
 // RUTAS
-
+import { APP_ROUTE } from './app.routes';
+//Modulos
+import { PagesModule } from './pages/pages.modulo';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTE
+    APP_ROUTE,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
